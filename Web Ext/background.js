7375eee1913d530ae54520f72b8a537b9f962926
Abhinav Raghuvanshi <78714364+abhinavr11@users.noreply.github.com>
2021-07-25 21:06:22 +0530

    Add files via upload

---
+++ Web Ext/background.js
@@ -0,0 +1,44 @@
+console.log("backgorund running");
+window.para = [];
+var url1 ;
+var post_url = 'http://127.0.0.1:5000/'
+chrome.runtime.onMessage.addListener(receiver);
+
+function receiver(request, sender, sendResponse)
+{    
+     if(request.receiver == 'background')
+    {
+        if(request.data[1].toString() != "null")
+        {
+        url1 = request.data[1].toString();
+        console.log(url1.toString());
+
+        sendResponse({});
+
+        chrome.tabs.create({url:url1},function(newTab) {
+
+        chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function() {
+            chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, { file: "payload.js" }, function() {});
+        });
+    
+    
+           
+            });
+        }
+        
+    }    
+    
+    if(request.receiver == 'sent_from_payload')
+    {
+        //para=request.data;
+        console.log('connecting with model');
+         $.post(post_url,JSON.stringify(request.data),function(data, status, res) {   para = data.results.results; console.log(data.results.results); })
+    }
+
+
+    sendResponse({});
+
+}
+
+
+
